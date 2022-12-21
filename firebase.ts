daa68bc2f2358c6035aec2725820b03753221178
hiteshlimbanidev <hiteshbusinessacc@gmail.com>
2022-12-22 00:24:47 +0530

    firebase

---
+++ firebase.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2FvKwrzeiEXWxGUv5D6-jslYzuqgl690",
+  authDomain: "signalclone-2ed76.firebaseapp.com",
+  projectId: "signalclone-2ed76",
+  storageBucket: "signalclone-2ed76.appspot.com",
+  messagingSenderId: "529728844733",
+  appId: "1:529728844733:web:466362977febfcf0e698d4",
+  measurementId: "G-YQ9X3BFZY3",
+};
